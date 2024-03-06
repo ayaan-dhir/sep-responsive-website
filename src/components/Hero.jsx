9e0e71f6927da195cca3185afdf9956196bde061
Ayaan Dhir <ayaand@uci.edu>
2024-03-06 00:56:52 -0800

    made some more progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { ReactTyped } from "react-typed";
 
 const Hero = () => {
-  return <div className="text-white">
-    <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-    <h1>Sigma Eta Pi</h1>
-    <div>
-        <p> insert ...</p>
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 sm:py-4 py-2">
+          Sigma Eta Pi
+        </h1>
+        <div className="flex justify-center items-center">
+          <ReactTyped
+            className="md:text-3xl sm:text-2xl text-xl font-bold text-[#38D6FA]"
+            strings={["Build.", "Connect.", "Launch."]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
+        </div>
+      </div>
     </div>
-    </div>
-  </div>;
+  );
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,47 @@ import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false); // Controls the visibility of the side menu
 
   const handleNav = () => {
-    setNav(!nav);
+    setNav(!nav); // Toggles the state, which in turn, controls the visibility
   };
 
   return (
-    <div className=" flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[white]">Sigma Eta Pi</h1>
+    <div
+      className="flex justify-between items-center h-24 mx-auto px-6 text-white"
+      style={{ position: "relative", zIndex: 1 }}
+    >
+      <h1
+        className="w-full text-3xl font-bold text-[white]"
+        style={{ whiteSpace: "nowrap" }}
+      >
+        Sigma Eta Pi
+      </h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Startups</li>
-        <li className="p-4">Members</li>
-        <li className="p-4" style={{ whiteSpace: "nowrap" }}>
+        <li className="p-4 text-xl">Startups</li>
+        <li className="p-4 text-xl">Members</li>
+        <li className="p-4 text-xl" style={{ whiteSpace: "nowrap" }}>
           Founder's Education
         </li>
-        <li className="p-4">Recruitment</li>
+        <li className="p-4 text-xl">Recruitment</li>
       </ul>
-      <div onClick={handleNav} className="block md:hidden">
-          {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+      <div
+        onClick={handleNav}
+        className="block md:hidden"
+        style={{ zIndex: 2, position: "relative" }}
+      >
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#191818] ease-in-out duration-500" : "fixed left-[-100%]"}>
-        <h1 className="w-full text-3xl font-bold text-[white] m-4">
-          Sigma Eta Pi
-        </h1>
-        <ul className="uppercase">
+      <div
+        className={
+          nav
+            ? "fixed right-0 top-0 w-[60%] h-full border-l border-l-gray-900 bg-[#191818] ease-in-out duration-500"
+            : "fixed right-[-100%] ease-in-out duration-500"
+        }
+        style={{ zIndex: 1 }}
+      >
+        <ul className="uppercase pt-6">
           <li className="p-4">Startups</li>
           <li className="p-4">Members</li>
           <li className="p-4" style={{ whiteSpace: "nowrap" }}>
