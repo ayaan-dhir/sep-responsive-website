398b6c5cdfbd2576e07fa84f9b499e0c4fbeabe6
Ayaan Dhir <ayaand@uci.edu>
2024-03-06 00:02:22 -0800

    made more progress

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return <div className="text-white">
+    <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+    <h1>Sigma Eta Pi</h1>
+    <div>
+        <p> insert ...</p>
+    </div>
+    </div>
+  </div>;
+};
+
+export default Hero;
