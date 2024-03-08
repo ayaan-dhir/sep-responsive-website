d8b4ed4ddeb82c110acc86cada245a5e9ed33d52
Ayaan Dhir <ayaand@uci.edu>
2024-03-07 23:49:21 -0800

    fixed a bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,17 +9,22 @@ const Navbar = () => {
     setNav(!nav);
   };
 
+  // New function to close the navbar
+  const closeNav = () => {
+    setNav(false);
+  };
+
   return (
     <div
       data-theme="forest"
       className="flex justify-between items-center h-24 mx-auto px-6"
       style={{ position: "relative", zIndex: 1 }}
     >
-      {/* Wrap "Sigma Eta Pi" text with Link */}
       <Link
         to="/"
         className="w-full text-3xl font-bold text-[white]"
         style={{ whiteSpace: "nowrap" }}
+        onClick={closeNav} // Close nav when clicking on the logo/home link
       >
         Sigma Eta Pi
       </Link>
@@ -50,23 +55,31 @@ const Navbar = () => {
       <div
         className={
           nav
-            ? "fixed right-0 top-0 w-[60%] h-full border-l border-l-gray-900 bg-[#191818] ease-in-out duration-500"
-            : "fixed right-[-100%] ease-in-out duration-500"
+            ? "fixed right-0 top-0 w-[50%] h-full border-l border-l-gray-900 bg-[#191818] ease-in-out duration-500"
+            : "fixed right-[-100%] top-0 w-[50%] h-full border-l border-l-gray-900 bg-[#191818] ease-in-out duration-500"
         }
         style={{ zIndex: 1 }}
       >
         <ul className="pt-6">
           <li className="p-4">
-            <Link to="/startups">Startups</Link>
+            <Link to="/startups" onClick={closeNav}>
+              Startups
+            </Link>
           </li>
           <li className="p-4">
-            <Link to="/members">Members</Link>
+            <Link to="/members" onClick={closeNav}>
+              Members
+            </Link>
           </li>
           <li className="p-4" style={{ whiteSpace: "nowrap" }}>
-            <Link to="/founders-education">Founder's Education</Link>
+            <Link to="/founders-education" onClick={closeNav}>
+              Founder's Education
+            </Link>
           </li>
           <li className="p-4">
-            <Link to="/recruitment">Recruitment</Link>
+            <Link to="/recruitment" onClick={closeNav}>
+              Recruitment
+            </Link>
           </li>
         </ul>
       </div>
