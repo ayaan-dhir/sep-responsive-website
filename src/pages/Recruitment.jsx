9cbbe41c195e5db9382a78b62d7cd67ae986ad1b
Ayaan Dhir <ayaand@uci.edu>
2024-03-26 22:24:49 -0600

    schedule added

--- src/pages/Recruitment.jsx
+++ src/pages/Recruitment.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import shreya from "../assets/members/board/shreya.jpg";
 import rise from "../assets/recruitment/rise-banner2.png";
+import schedule from "../assets/recruitment/schedule.png";
 import { HoverBorderGradient } from "../components/ui/hover-border-gradient.tsx";
 
 const Recruitment = () => {
@@ -25,11 +26,7 @@ const Recruitment = () => {
         <h1 className="text-white md:text-7xl sm:text-6xl text-4xl font-bold md:py-10 sm:py-4 py-2">
           The Schedule
         </h1>
-        <img
-          className="w-[80%] object-[50%_50%] sm:object-[50%_50%] md:object-[30%_50%]"
-          src={rise}
-          alt="Rise"
-        />
+        <img className="sm:w-[70%] w-[90%]" src={schedule} alt="Schedule" />
       </div>
       <div className="sm:snap-start flex flex-col justify-center items-center mx-auto sm:w-[90%] w-full h-screen text-center">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mt-20 md:mt-20 lg:mt-0 xl:mt-0 w-full max-w-4xl">
