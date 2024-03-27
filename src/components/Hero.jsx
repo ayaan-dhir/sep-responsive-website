b073a41d466ad57a08795421e66f66a69be36320
Ayaan Dhir <ayaand@uci.edu>
2024-03-26 22:39:04 -0600

    fixed overlap bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <div className="h-screen w-full bg-black relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-4xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold p-2">
+        <h1 className="relative z-1 sm:z-10 text-4xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold p-2">
           Sigma Eta Pi
         </h1>
       </div>
