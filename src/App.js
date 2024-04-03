9b2e0f55a59a2c63aba87d85a5c36c3f5022885b
Ayaan Dhir <ayaand@uci.edu>
2024-04-02 17:27:43 -0700

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Stats from "./components/Stats";
-import Startups from "./pages/Startups"
+import Startups from "./pages/Startups";
 import Board from "./pages/Board";
-import Classes from "./pages/Classes"
+import Classes from "./pages/Classes";
 import FoundersEducation from "./pages/FoundersEd";
 import Recruitment from "./pages/Recruitment";
 import Community from "./pages/Community";
+import { useLocation } from "react-router-dom";
 
+// ScrollToTop component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
     <Router>
       <div data-theme="black">
+        <ScrollToTop /> {/* Include the ScrollToTop component here */}
         <Navbar />
         <Routes>
           <Route
