5136d9567770f8d7567d8676a58c07e8ffe3a888
rohitde <de.rohit01@gmail.com>
2024-07-12 03:14:05 -0700

    recruitment adjusted, interested button stationary

--- src/pages/Recruitment.jsx
+++ src/pages/Recruitment.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { HoverBorderGradient } from "../components/ui/hover-border-gradient.tsx";
-import backgroundImage from "../assets/community/utahSquad.JPG"; // Ensure the path is correct
+import backgroundImage from "../assets/community/utahSquad.JPG";
 
 const Recruitment = () => {
-  const navbarHeight = 0; // Adjust this value to the height of your navbar
+  // adjust this value to the height of the navbar, just keep it at 0
+  const navbarHeight = 0;
+  // adjust this value to move the background down
+  const backgroundOffset = -126;
 
   return (
     <div
-      className="sm:overflow-y-scroll sm:snap-mandatory sm:snap-y sm:h-screen"
+      className="sm:overflow-y-scroll sm:snap-mandatory sm:snap-y sm:h-screen relative"
       style={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        paddingTop: `${navbarHeight}px`, // Add top padding equal to the height of the navbar
+        backgroundPosition: `center ${backgroundOffset}px`,
+        paddingTop: `${navbarHeight}px`, // add top padding equal to the height of the navbar
         height: `calc(100vh - ${navbarHeight}px)`,
       }}
     >
-      <div className="snap-start flex flex-col mx-auto text-center justify-center items-center h-full">
+      <div className="snap-start flex flex-col mx-auto text-center justify-center items-center h-full absolute top-0 left-0 right-0 bottom-0">
         <div className="flex justify-center items-center pt-2 pb-2 space-x-4">
           <div className="md:z-10 z-0">
             <HoverBorderGradient>
