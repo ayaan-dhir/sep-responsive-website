d2c2a6c76a3cb2a2a87b14525b096111fbc47c92
rohitde <de.rohit01@gmail.com>
2024-07-12 01:01:08 -0700

    added linkedin link to images for zeta

--- src/pages/Zeta.jsx
+++ src/pages/Zeta.jsx
@@ -13,48 +13,55 @@ const zetaData = [
     id: 1,
     name: "Hoon Kim",
     photo: hoon,
+    link: "https://www.linkedin.com/in/hoononthemoon",
   },
 
   {
     id: 2,
     name: "Rohit De",
     photo: rohit,
+    link: "https://www.linkedin.com/in/rohitde/",
   },
 
   {
     id: 3,
     name: "Kyle King",
     photo: kyle,
+    link: "https://www.linkedin.com/in/king-kyle/",
   },
 
   {
     id: 4,
     name: "Yurina Tamura",
     photo: yurina,
+    link: "http://www.linkedin.com/in/yurina-tamura",
   },
 
   {
     id: 5,
     name: "Richard Hunt",
     photo: richard,
+    link: "https://www.linkedin.com/in/richardcarlhunt/",
   },
 
   {
     id: 6,
     name: "Jose Callejas",
     photo: jose,
+    link: "https://www.linkedin.com/in/jose-mizraim-callejas-gonzalez-371987212/",
   },
-]
+];
 
 function Zeta() {
   return (
-      <div data-theme="black" className="text-center">
-        <h1 className="font-semibold text-[56pt] text-white pt-[96px]">
-          Zeta
-        </h1>
-        <div className="grid justify-center grid-cols-[repeat(auto-fill,minmax(250px,1fr))] lg:gap-x-[150px] lg:gap-y-[100px] gap-[100px] pl-[100px] pr-[100px] pt-[30px] pb-[50px]">
-          {zetaData.map((member) => (
-            <div key={member.id} className="flex flex-col items-center">
+    <div data-theme="black" className="text-center">
+      <h1 className="font-semibold text-[56pt] text-white pt-[96px]">
+        Zeta
+      </h1>
+      <div className="grid justify-center grid-cols-[repeat(auto-fill,minmax(250px,1fr))] lg:gap-x-[150px] lg:gap-y-[100px] gap-[100px] pl-[100px] pr-[100px] pt-[30px] pb-[50px]">
+        {zetaData.map((member) => (
+          <div key={member.id} className="flex flex-col items-center">
+            <a href={member.link} target="_blank" rel="noopener noreferrer">
               <img
                 src={member.photo}
                 alt={member.name}
@@ -62,13 +69,14 @@ function Zeta() {
                 effect="blur"
                 className="w-[300px] h-[400px] rounded-[20px] object-cover"
               />
-              <div className="font-normal text-[18pt] mt-[10px] text-white">
-                {member.name}
-              </div>
+            </a>
+            <div className="font-normal text-[18pt] mt-[10px] text-white">
+              {member.name}
             </div>
-          ))}
-        </div>
-        </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
